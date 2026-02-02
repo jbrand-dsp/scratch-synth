d543135b73a0d42149bd41cf4abce7ed6d885003
jbrand-dsp <jdmbrandis@gmail.com>
2026-02-02 18:27:30 +0100

    added midi parsing logic

--- midi.h
+++ midi.h
@@ -35,32 +35,27 @@ uint32_t read_uint32_be(const uint8_t *buf) {
          ((uint32_t)buf[2] << 8) | ((uint32_t)buf[3]);
 }
 
-// track_header parseTrackHeader(FILE *file) {
-//   uint8_t buffer[MIDI_TRACK_HEADER_SIZE];
-//   fread(buffer, 1, 2, file);
-
-//   track_header h;
-//   memcpy(h.MTrk, buffer, 4);
-//   h.track_length = swap32((uint32_t *)(buffer + 4));
-// }
-
 typedef enum {
   MIDI_VLQ_DATA_MASK = 0x7F,   // lower 7 bits contain the value
   MIDI_VLQ_CONTINUATION = 0x80 // MSB = 1 means more bytes follow
 } midi_vlq_flags_t;
 
-// midi_header parseMidiHeader(FILE *file) {
-//   uint8_t buffer[MIDI_HEADER_SIZE];
-//   fread(buffer, 1, 14, file);
+typedef struct {
+  uint8_t event_type;
+  uint8_t midi_channel;
+  uint8_t param_1;
+  uint8_t param_2;
+} midi_event;
 
-//   //MIDI header is encoded in Big Endian hence the conversion to little
-//   Endian midi_header h; memcpy(h.MThd, buffer, 4); h.header_length =
-//   read_uint16_be(buffer + 4); //Is 4 this correct?? h.format =
-//   read_uint16_be(buffer + 8); h.num_tracks = read_uint16_be(buffer + 10);
-//   h.division = read_uint16_be(buffer + 12);
-
-//   return h;
-// }
+typedef enum {
+  MIDI_NOTE_OFF = 0x80,
+  MIDI_NOTE_ON = 0x90,
+  MIDI_POLY_PRESSURE = 0xA0,
+  MIDI_CONTROL_CHANGE = 0xB0,
+  MIDI_PROGRAM_CHANGE = 0xC0,
+  MIDI_CHANNEL_PRESS = 0xD0,
+  MIDI_PITCH_BEND = 0xE0,
+} midi_event_type;
 
 void parseMidiFIle(FILE *file) {
   uint8_t buffer[MIDI_HEADER_SIZE];
@@ -91,13 +86,14 @@ void parseMidiFIle(FILE *file) {
 
       // read first bite
       fread(stream, 1, 1, file);
-      int n = 0;
+      int n = 0; // TODO: check if n always 0
       int d_time = 0;
       int value = 0;
       int byte = stream[n];
       value = byte & MIDI_VLQ_DATA_MASK;
       d_time = (d_time << 7) | value; // (d_time * 128) + value
       num_bytes_consumed_by_delta_time++;
+
       // continue reading if MSB == 1
       while (byte & MIDI_VLQ_CONTINUATION) {
         fread(stream, 1, 1, file);
@@ -107,22 +103,108 @@ void parseMidiFIle(FILE *file) {
         num_bytes_consumed_by_delta_time++;
       }
 
+      /**
+      typedef struct {
+        uint8_t event_type;
+        uint8_t midi_channel;
+        uint8_t param_1;
+        uint8_t param_2;
+      } midi_event;
+       */
+
       // parse midi
+      fread(stream, 1, 1, file);
+      num_bytes_consumed_by_event++;
+      byte = stream[0];
+      uint8_t captured_midi_event = byte;
+      uint8_t midi_channel = 0;
+      uint8_t MIDI_CHANNEL_MASK = 0x0F;
+      midi_event m;
+      m.midi_channel = 0;
+      m.param_1 = 0;
+      m.param_2 = 0;
+
+      if (captured_midi_event & MIDI_NOTE_OFF) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+      }
+
+      if (captured_midi_event & MIDI_NOTE_ON) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
+      if (captured_midi_event & MIDI_POLY_PRESSURE) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
+      if (captured_midi_event & MIDI_CONTROL_CHANGE) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
+      if (captured_midi_event & MIDI_PROGRAM_CHANGE) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
+      if (captured_midi_event & MIDI_CHANNEL_PRESS) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
+      if (captured_midi_event & MIDI_PITCH_BEND) {
+        m.midi_channel = byte & MIDI_CHANNEL_MASK;
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_1 = stream[0];
+        fread(stream, 1, 1, file);
+        num_bytes_consumed_by_event++;
+        m.param_2 = stream[0];
+        break;
+      }
+
       bytes_remaining -= num_bytes_consumed_by_delta_time;
       bytes_remaining -= num_bytes_consumed_by_event;
     }
   }
 }
-
-enum {
-  MIDI_NOTE_OFF = 0x80,
-  MIDI_NOTE_ON = 0x90,
-  MIDI_POLY_PRESSURE = 0xA0,
-  MIDI_CONTROL_CHANGE = 0xB0,
-  MIDI_PROGRAM_CHANGE = 0xC0,
-  MIDI_CHANNEL_PRESS = 0xD0,
-  MIDI_PITCH_BEND = 0xE0,
-};
 
 /**
  *
